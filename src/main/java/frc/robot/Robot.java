d02bc11cee0c8275189c2a3df78f515f061e8d13
rishi <rveerepalli>
2019-09-19 17:30:37 -0700

    add all hardware devices and write controls

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,37 +15,76 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 public class Robot extends TimedRobot {
 
     /* initialize the joystick */
-    private Joystick m_joystick;
+    private Joystick joystick;
 
-    /* initialize every talon */
-    public TalonSRX drivetrainLeft;
-    public TalonSRX drivetrainRight;
+    /* create objects for every talon */
+    private TalonSRX flywheelOne;
+    private TalonSRX flywheelTwo;
+    private TalonSRX drivetrainLeft;
+    private TalonSRX drivetrainRight;
+    private TalonSRX shooter;
+    private TalonSRX actuator;
+
+    private double flywheelspeed = 0;
     
     private static final enum Controls {
 	A=1, B, X, Y, LB, RB, BACK, START, LEFT_JOYSTICK_PRESSED, RIGHT_JOYSTICK_PRESSED
     };
 
     /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
+     * Initialization process
      */
     @Override
     public void robotInit() {
 
-	/* assign a port to the joystick */
-	m_joystick = new Joystick(0);
+        /* assign a port to the joystick */
+	joystick = new Joystick(0);
 	
 	/* assign talon ports */
+	flywheelOne = new TalonSRX(0);
+	flywheelTwo = new TalonSRX(1);
 	drivetrainLeft = new TalonSRX(2);
 	drivetrainRight = new TalonSRX(3);
-
+	shooter = new TalonSRX(4);
+	actuator = new TalonSRX(5);
+	
     }
-    
+
+    /**
+     * Send packets every time interval.
+     */
     @Override
     public void teleopPeriodic() {
 
-	drivetrainLeft.set(ControlMode.PercentOutput, -(m_joystick.getRawAxis(1)));
-        drivetrainRight.set(ControlMode.PercentOutput, m_joystick.getRawAxis(5));
+	/* tank drive */
+	drivetrainLeft.set(ControlMode.PercentOutput, -(joystick.getRawAxis(1)));
+        drivetrainRight.set(ControlMode.PercentOutput, joystick.getRawAxis(5));
+
+	/* use the dpad to elevate and lower the shooter */
+	while (joystick.getPOV() == 0) {
+	    actuator.set(ControlMode.PercentOutput, 100);
+	}
+	while (joystick.getPOV() == 180) {
+	    actuator.set(ControlMode.PercentOutput, 0);
+	}
+
+	/* turn on the flywheels */
+	if (joystick.getRawButton(Controls.START)) {
+	    flywheelOne.set(ControlMode.PercentOutput, (flywheelspeed)*100)
+	}
+
+	/* reduce and increase flywheel speed */
+	if (joystick.getRawButton(LB) && flywheelspeed > 0) {
+	    flywheelspeed = flywheelspeed - 0.2;
+	} else if (joystick.getRawButton(LB) && flywheelspeed < 1.0) {
+	    flywheelspeed = flywheelspeed + 0.2;
+	}
+
+	/* turn off flywheels */
+	if (m_joystick.getRawButton(Controls.BACK)) {
+	    flywheelOne.set(ControlMode.PercentOutput, 0);
+	    flywheelTwo.set(ControlMode.PercentOutput, 0);
+	}
 	
     }
     
